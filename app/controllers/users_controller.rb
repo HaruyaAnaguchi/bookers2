82816523707670a5f462378596f40512717a06d0
HaruyaAnaguchi <131515772+HaruyaAnaguchi@users.noreply.github.com>
2023-06-27 09:15:55 +0000

    [Fix]second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,6 @@ class UsersController < ApplicationController
 
   private
    def user_params
-     params.require(:user).permit(:name, :introduction, :profile_iamge)
+     params.require(:user).permit(:name, :introduction, :profile_image)
    end
 end
